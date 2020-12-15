d895200103cdfbeafc0237a770595fec912af516
menyhartsari <63165278+menyhartsari@users.noreply.github.com>
2020-12-15 13:51:38 +0100

    Add files via upload

---
+++ postfix.py
@@ -0,0 +1,80 @@
+def eval_expr(s,d={}):
+	list=[]
+	s=s.split()
+	for i in s:
+		if i=="+":
+			A=list.pop()
+			B=list.pop()
+			C=A+B
+			list.append(C)
+		elif i=="-":
+			A=list.pop()
+			B=list.pop()
+			C=B-A
+			list.append(C)
+		elif i=="*":
+			A=list.pop()
+			B=list.pop()
+			C=A*B
+			list.append(C)
+		elif i=="/":
+			A=list.pop()
+			B=list.pop()
+			C=B//A
+			list.append(C)
+		elif i=="x":
+			i=d["x"]
+			i=int(i)
+			list.append(i)
+		elif i=="y":
+			i=d["y"]
+			i=int(i)
+			list.append(i)
+		else:
+			i=int(i)
+			list.append(i)
+	return list.pop()
+
+def to_infix(s):
+	s=s.split()
+	list=[]
+	for i in s:
+		if i=="+":
+			A=list.pop()
+			B=list.pop()
+			list.append('( '+B+' '+i+' '+A+' )')
+		elif i=="-":
+			A=list.pop()
+			B=list.pop()
+			list.append('( '+B+' '+i+' '+A+' )')
+		elif i=="*":
+			A=list.pop()
+			B=list.pop()
+			list.append('( '+B+' '+i+' '+A+' )')
+		elif i=="/":
+			A=list.pop()
+			B=list.pop()
+			list.append('( '+B+' '+i+' '+A+' )')
+		else:
+			list.append(i)
+	return list.pop()
+
+
+def to_postfix(s):
+	s=s.split()
+	list1=[]
+	list2=[]
+	for i in s:
+		if i=="(":
+			continue
+		if i==")":
+			A=list1.pop()
+			B=list1.pop()
+			C=list2.pop()
+			list1.append(B+' '+A+' '+C)
+		elif i=="+" or i=="-" or i=="*" or i=="/":
+			list2.append(i)
+		else:
+			list1.append(i)
+	return list1.pop()
+			
